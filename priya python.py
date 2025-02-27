f68509c245c6a55c576f28d98dc98f08fe90677f
pavinai <pavinraj003@gmail.com>
2025-02-27 12:00:16 +0530

    Add files via upload

---
+++ priya python.py
@@ -0,0 +1,10 @@
+a=int(input("enter a value of a:"))
+b=int(input("enter a value of b:"))
+c=int(input("enter a value of c:"))
+if(a>b) and (a>c):
+     largest="a is largest value" 
+elif (b>c):
+     largest="b is largest  value"
+else :
+    largest=" c   is largest value "
+print( "the largest value is ",largest)
